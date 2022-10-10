58e44457c2b37688e1359e84aa9b44582067c960
Jan Szczepan <janszczepan62@gmail.com>
2022-10-10 18:45:15 +0200

    Authorization middleware

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken'
+
+import UserModel from '../models/user.js'
+
+const requieAuth = async (req, res, next) => {
+   const {authorization} = req.headers
+
+   if(!authorization)
+      return res.status(401).json({error: 'Authorization token required'})
+
+   const token = authorization.split(' ')[1]
+   
+   try {
+      const {_id} = jwt.verify(token, process.env.SECRET)
+
+      req.user = await UserModel.findOne({_id}).select('_id')
+
+      next()
+   } catch (error) {
+      console.log(error)
+
+      res.status(401).json({error: 'Request is not authorized'})
+   }
+}
+
+module.exports = requieAuth
