1da5b401565ee42de02188a853a22f8a66730c3d
Jan Szczepan <janszczepan62@gmail.com>
2022-10-22 21:42:13 +0200

    return Local Date bug fix

--- frontend/src/functions/returnDate.js
+++ frontend/src/functions/returnDate.js
@@ -3,11 +3,11 @@ const returnDate = (date) => {
    if (!date) return {}
 
    const months = ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień']
-   const monthNumber = (new Date(date)).getMonth() + 1
+   const monthNumber = (new Date(date)).getUTCMonth() + 1
    const month = months[monthNumber - 1]
-   const day = (new Date(date)).getDate()
-   let hours = (new Date(date)).getHours()
-   let minutes = (new Date(date)).getMinutes()
+   const day = (new Date(date)).getUTCDate()
+   let hours = (new Date(date)).getUTCHours()
+   let minutes = (new Date(date)).getUTCMinutes()
 
    if (hours < 10)
       hours = '0' + hours
