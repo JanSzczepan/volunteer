3ff7d8794d99c9f54b9363316bce61f13d63d2dd
Jan Szczepan <janszczepan62@gmail.com>
2022-10-22 12:46:38 +0200

    make it better

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,10 @@ app.use('/events', eventRoutes)
 app.use('/collection', collectionRoutes)
 app.use('/user', userRoutes)
 
+app.get('/', (req, res) => {
+   res.send('APP IS RUNNING.')
+})
+
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(process.env.MONGO_URI)
